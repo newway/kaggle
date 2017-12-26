694d8fe54c93541739b695d29eb96434a759f096
liuwei <liuwei@newway.local>
2017-12-26 15:51:11 +0800

    imputed, mangled, encoded for data feature selection for mode: SelectKBest, SelectFromModel ensemble, grid search for Hyper-Parameters, cross-validation for mode
    TODO:	PCM to reduce feature dimensionality, RFECV for feature selection

---
+++ classifiers.py
@@ -0,0 +1,220 @@
+
+import pandas as pd
+import numpy as np
+#Common Model Algorithms
+from sklearn import svm, tree, linear_model, neighbors, naive_bayes, ensemble, discriminant_analysis, gaussian_process
+import xgboost as xgb
+#Common Model Helpers
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn import feature_selection
+from sklearn.feature_selection import SelectFromModel
+from sklearn import model_selection
+from sklearn import metrics
+
+#Machine Learning Algorithm (MLA) Selection and initialization
+CLF = [
+    #Ensemble Methods
+    ('ada', ensemble.AdaBoostClassifier(tree.DecisionTreeClassifier())),
+    ('bc', ensemble.BaggingClassifier()),
+    ('etc',ensemble.ExtraTreesClassifier()),
+    ('gbc', ensemble.GradientBoostingClassifier()),
+    ('xgbc',xgb.XGBClassifier()),    #max_depth=5, n_estimators=50, learning_rate=0.05)
+    ('rfc', ensemble.RandomForestClassifier(n_estimators = 50)),
+
+    #Gaussian Processes
+    ('gpc', gaussian_process.GaussianProcessClassifier()),
+    
+    #GLM - remove linear models, since this is a classifier algorithm
+    ('lr', linear_model.LogisticRegressionCV()),
+    ('pac', linear_model.PassiveAggressiveClassifier()),
+    ('rc', linear_model. RidgeClassifierCV()),
+    ('sgd', linear_model.SGDClassifier()),                                                                                      
+    ('pct', linear_model.Perceptron()),                                                                                          
+    
+    #Navies Bayes
+    ('gnb', naive_bayes.GaussianNB()),
+    
+    #Nearest Neighbor
+    ('knn', neighbors.KNeighborsClassifier(n_neighbors = 3)),
+    
+    #SVM
+    ('svc', svm.SVC(probability=True)),
+    ('lsvc', svm.LinearSVC()),
+    
+    #Trees    
+    ('dtc', tree.DecisionTreeClassifier()),
+    ('etc2', tree.ExtraTreeClassifier()),
+]
+
+EST_PARAM_GRID = {  #'ada':  [{"base_estimator__criterion": ["gini", "entropy"],         #0
+                    #                        "base_estimator__splitter":   ["best", "random"],
+                    #                        #'base_estimator__max_depth': [1, 2],
+                    #                        #'base_estimator__min_samples_split': [5,10,20,50,70],
+                    #                        #'base_estimator__max_features': [None, 'auto', 'sqrt', 'log2'],
+                    #                        "learning_rate":[0.01,0.05,0.1,0.5,1],
+                    #                        "n_estimators": [5,20,30,50,70],
+                    #                        "algorithm": ['SAMME', 'SAMME.R'],
+                    #                        "random_state":[None, 0]}],
+                    #'etc':  [{  "max_depth": [None],
+                    #            "max_features": [3,4,5,8,9],
+                    #            "min_samples_split": [2, 3, 5, 10, 20,50],
+                    #            "min_samples_leaf": [1, 3, 10,20],
+                    #            "bootstrap": [False],
+                    #            "n_estimators" :[30,50,100,300],
+                    #            "criterion": ["gini"]}],     
+                    #'gbc':  [{  'loss' : ["deviance"],
+                    #            'n_estimators' : [50,100,200,300],
+                    #            'learning_rate': [0.5, 0.1, 0.05, 0.01],
+                    #            'max_depth': [3,4,5, 8],
+                    #            'min_samples_leaf': [30,50,70,100,150],
+                    #            'max_features': [0.3, 0.1,0.5]}],
+                    'xgbc': [{  "max_depth":[3,4,5],
+                                "n_estimators":[10,20,30,40,50,60,70,80,100],
+                                "learning_rate":[0.01,0.05,0.1,0.5,1]}],
+                    #'rfc':  [{  "max_depth": [3],
+                    #            "max_features": [0.2,0.3,0.4,0.5,0.6,0.8],
+                    #            "min_samples_split": [5, 10, 20, 50, 100],
+                    #            "min_samples_leaf": [3, 5, 10, 20, 50, 100],
+                    #            "bootstrap": [False],
+                    #            "n_estimators" :[30,50,80,100],
+                    #            "criterion": ["gini"]}],
+                    #'gpc':  [{"kernel":[None], "random_state":[None, 0]}],          #no SelectFromModel http://scikit-learn.org/stable/modules/generated/sklearn.gaussian_process.GaussianProcessClassifier.html#sklearn.gaussian_process.GaussianProcessClassifier
+                    #'lr':   [{'solver':['lbfgs', 'liblinear']}],  #http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegressionCV.html#sklearn.linear_model.LogisticRegressionCV
+                    #'gnb':  [{'priors':[None]}],                #no SelectFromModel because has no `coef_` or `feature_importances_` attribute
+                    #'knn':  [{'n_neighbors':[3,5,7,11]}],          #no SelectFromModel
+                    #'svc':  [{  'kernel': ['rbf'],                 #no SelectFromModel 
+                    #            'gamma': [ 0.001, 0.01, 0.1, 1],   
+                    #            'C': [1, 10, 50, 100,200,300, 1000]}],
+                    #'dtc':  [{  'criterion': ['gini', 'entropy'],
+                    #            'splitter': ['best', 'random'],
+                    #            'max_depth': [None, 2,4,6,8],
+                    #            'min_samples_split': [2,5,10,20,50,70],
+                    #            'max_features': [None, 0.3, 'auto', 'sqrt', 'log2']}],   #auto means max_features=sqrt(n_features); None: then max_features=n_features
+                    #'etc2': [{  "max_depth": [None],
+                    #            "max_features": [3, 4,5,6],
+                    #            "min_samples_split": [2, 3, 10, 20, 50],
+                    #            "min_samples_leaf": [1, 3, 10],
+                    #            "criterion": ["gini"]}],
+
+}
+
+def get_feature_by_mode(alg, data, target, all_columns, prefit=False, threshold=None):
+    sfMode = SelectFromModel(alg, prefit=False, threshold=None)       #The estimator must have either a feature_importances_ or coef_ attribute after fitting, threshold:"0.1*mean" or 0.25 
+    sfMode = sfMode.fit(data, target)
+    selected_columns = sfMode.get_support(indices=True).tolist()     #list of index
+    n_features = len(selected_columns)
+    print(selected_columns)
+    #while n_features <= 2:
+    #    sfMode.threshold -= 0.1
+    #    data_S = sfMode.transform(data)
+    #    n_features = data_S.shape[1]
+    #print('\n', name, "------SelectFromModel:", n_features, all_columns, selected_columns)
+    print(alg.__class__.__name__, "------SelectFromModel:", n_features, [all_columns[x] for x in selected_columns])
+    return (sfMode, selected_columns)
+
+MLA_columns = ['MLA Name', 'MLA Test Accuracy Mean', 'MLA Train Accuracy Mean', 'Test Train Diff', 'MLA Test Accuracy -3Std', 'MLA Parameters', 'Selected Columns', 'MLA Fit Time', 'TrainData','TestData','MLA Mode']
+
+def ensemble_model(data, target, test, all_columns, featureFromModel=False, n_splits=10, testpart=0.3, trainpart=0.6, **kw):
+    #note: this is an alternative to train_test_split
+    #cv_split = 3  #When the cv argument is an integer, cross_val_score uses the KFold or StratifiedKFold strategies by default,
+    #cv_split = model_selection.StratifiedKFold(n_splits=10)
+    cv_split = model_selection.ShuffleSplit(n_splits = n_splits, test_size = testpart, train_size = trainpart, random_state = 0 ) # run model 10x with 60/30 split intentionally leaving out 10
+
+    #create table to compare MLA
+    MLA_compare = pd.DataFrame(columns = MLA_columns)
+
+    #index through MLA and save performance to table
+    row_index = 0
+    voting_ests = []
+    for est in CLF:
+        name, alg = est
+        if name not in EST_PARAM_GRID:
+            continue 
+        
+        if featureFromModel:
+            sfMode, selected_columns = get_feature_by_mode(alg, data, target, all_columns)
+            MLA_compare.loc[row_index, 'TrainData'] = sfMode.transform(data)
+            MLA_compare.loc[row_index, 'TestData'] = sfMode.transform(test)
+            MLA_compare.loc[row_index, 'Selected Columns'] = [ all_columns[i] for i in selected_columns]
+        else:
+            MLA_compare.loc[row_index, 'TrainData'] = data
+            MLA_compare.loc[row_index, 'TestData'] = test
+            MLA_compare.loc[row_index, 'Selected Columns'] = all_columns
+        #print(data_S[:2,:])
+        param_grid = EST_PARAM_GRID[name]
+        n_features = len(MLA_compare.loc[row_index]['Selected Columns'])
+        for pg in param_grid:
+            if 'max_features' in pg:
+                max_feature_l = pg['max_features']
+                pg['max_features'] = [x for x in max_feature_l if not isinstance(x, int) or x<=n_features ]       #filter(lambda x: x<=len(n_features), max_feature_l)
+        #score model with cross validation: http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.cross_validate.html#sklearn.model_selection.cross_validate
+        #cv_results = model_selection.cross_validate(alg, data, target, cv=3, return_train_score=True)
+        #print(name, param_grid, '\n')
+        tune_model = model_selection.GridSearchCV(alg, param_grid=param_grid, scoring = 'accuracy', cv=cv_split, return_train_score=True, n_jobs= 4)
+        tune_model = tune_model.fit(MLA_compare.loc[row_index, 'TrainData'], target)
+        #print("before rfe: ", tune_model.best_params_)
+        #feature selection
+        #model_rfe = feature_selection.RFECV(tune_model.best_estimator_, step = 1, scoring = 'roc_auc', cv = cv_split)
+        #model_rfe.fit(data, target)
+        #train_rfe = model_rfe.transform(data)
+        #test_rfe = model_rfe.transform(test)
+        #rfe_columns = data.columns.values[model_rfe.get_support()]
+        # refit model with rfe columns
+        #tune_model = tune_model.fit(train_rfe, target)
+        #print("after rfe: ", tune_model.best_params_)
+        cv_results = tune_model.cv_results_
+        best_index = tune_model.best_index_     # index of best param
+        #cv_results = model_selection.cross_validate(tune_model.best_estimator_, data, target, cv=cv_split, return_train_score=True)
+        #test_means = tune_model.cv_results_['mean_test_score']  # list of all combined parameters mean  test score
+        #print(test_means, np.mean(test_means), max(test_means), test_means[best_index], "std for best??:", cv_results.get('std_test_score')[best_index])
+        #set name and parameters
+        MLA_compare.loc[row_index, 'MLA Parameters'] = str(tune_model.best_params_).replace(" ","")
+        MLA_compare.loc[row_index, 'MLA Name'] = alg.__class__.__name__
+        MLA_compare.loc[row_index, 'MLA Mode'] = tune_model.best_estimator_
+        MLA_compare.loc[row_index, 'MLA Fit Time'] = cv_results.get('mean_fit_time')[best_index]
+        MLA_compare.loc[row_index, 'MLA Train Accuracy Mean'] = cv_results.get('mean_train_score')[best_index]
+        MLA_compare.loc[row_index, 'MLA Test Accuracy Mean'] = cv_results.get('mean_test_score')[best_index]   # == tune_model.best_score_
+        MLA_compare.loc[row_index, 'Test Train Diff'] = MLA_compare.loc[row_index, 'MLA Test Accuracy Mean'] - MLA_compare.loc[row_index, 'MLA Train Accuracy Mean']
+        MLA_compare.loc[row_index, 'MLA Test Accuracy -3Std'] = -3*cv_results.get('std_test_score')[best_index]   #let's make -3Std minus for ascending order
+        #MLA_compare.loc[row_index, 'Selected Columns'] = all_columns
+        #MLA_compare.loc[row_index, 'TestData'] = test
+        
+        voting_ests.append((name, tune_model.best_estimator_))
+        row_index+=1
+    
+    if len(voting_ests) >= 3:
+        test_mean = MLA_compare['MLA Test Accuracy Mean'].mean()
+        voting_ests = [voting_ests[i] for i in range(MLA_compare.shape[0]) if MLA_compare.loc[i, 'MLA Test Accuracy Mean']>=test_mean]
+        print('all voting ests:\n', voting_ests)
+        alg = ensemble.VotingClassifier(voting_ests)
+        param_grid = {'voting':['hard', 'soft']}
+        tune_model = model_selection.GridSearchCV(alg, param_grid=param_grid, scoring = 'accuracy', cv=cv_split, return_train_score=True, n_jobs=4)
+        tune_model = tune_model.fit(data, target)
+        cv_results = tune_model.cv_results_
+        best_index = tune_model.best_index_
+        #set name and parameters
+        MLA_compare.loc[row_index, 'MLA Parameters'] = str(tune_model.best_params_).replace(" ","")
+        MLA_compare.loc[row_index, 'MLA Name'] = alg.__class__.__name__
+        MLA_compare.loc[row_index, 'MLA Mode'] = tune_model.best_estimator_
+        MLA_compare.loc[row_index, 'MLA Fit Time'] = cv_results.get('mean_fit_time')[best_index]
+        MLA_compare.loc[row_index, 'MLA Train Accuracy Mean'] = cv_results.get('mean_train_score')[best_index]
+        MLA_compare.loc[row_index, 'MLA Test Accuracy Mean'] = cv_results.get('mean_test_score')[best_index]
+        MLA_compare.loc[row_index, 'Test Train Diff'] = MLA_compare.loc[row_index, 'MLA Test Accuracy Mean'] - MLA_compare.loc[row_index, 'MLA Train Accuracy Mean']
+        MLA_compare.loc[row_index, 'MLA Test Accuracy -3Std'] = -3*cv_results.get('std_test_score')[best_index]   #let's make -3Std minus for ascending order
+        MLA_compare.loc[row_index, 'TrainData'] = data
+        MLA_compare.loc[row_index, 'TestData'] = test
+        MLA_compare.loc[row_index, 'Selected Columns'] = all_columns
+        row_index+=1
+
+    # get the best mode
+    MLA_compare.sort_values(by = ['MLA Test Accuracy Mean', 'Test Train Diff', 'MLA Test Accuracy -3Std'], ascending = False, inplace = True)
+    print_columns = MLA_columns[:-4]
+    #print_columns.remove("MLA Parameters")
+    print(MLA_compare[print_columns], '\n')
+    print(MLA_compare.iloc[0]["MLA Parameters"])
+    #loc[0]: 原始index为0，by label;    iloc[0]:当前index为0，by postion
+    #MLA_compare.head(n=1) return DataFrame, MLA_compare.loc[alg_index] return Series
+    ensemble_name = MLA_compare.iloc[0]['MLA Name'][:5] + MLA_compare.iloc[0]['MLA Parameters'].replace(':','_') + "_f{}".format(len(MLA_compare.iloc[0]['Selected Columns']))
+    model = MLA_compare.iloc[0]["MLA Mode"]
+    test_data = MLA_compare.iloc[0]['TestData']
+    return (model, model.predict(test_data), ensemble_name)
--- titanic/script.py
+++ titanic/script.py
@@ -4,12 +4,262 @@
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+import xgboost as xgb
+from sklearn import svm, tree, linear_model, neighbors, naive_bayes, ensemble, discriminant_analysis, gaussian_process
+from sklearn.model_selection import cross_val_score
+#from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+import re
+from sklearn.feature_selection import SelectKBest, f_classif, chi2
+import sys
+sys.path.append('..')
+from classifiers import ensemble_model
 # Input data files are available in the "../input/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
 from subprocess import check_output
-print(check_output(["ls", "../input"]).decode("utf8"))
 
-# Any results you write to the current directory are saved as output.
-#test = pd.read_csv("")
+from sklearn.base import TransformerMixin
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from collections import Counter
+
+def fix_outliers(df, col):
+    """
+    Takes a dataframe df of features and returns a list of the indices
+    corresponding to the observations containing more than n outliers according
+    to the Tukey method.
+    """
+    outlier_indices = []
+
+    # iterate over features(columns)
+    # 1st quartile (25%)
+    Q1 = np.percentile(df[col], 25)
+    # 3rd quartile (75%)
+    Q3 = np.percentile(df[col],75)
+    # Interquartile range (IQR)
+    IQR = Q3 - Q1
+
+    # outlier step
+    outlier_step = 1.5 * IQR
+
+    # Determine a list of indices of outliers for feature col
+    outlier_indices = df[(df[col] < Q1 - outlier_step) | (df[col] > Q3 + outlier_step )].index
+    print(outlier_indices)
+    for i in outlier_indices:
+        print("original:", df.iloc[i][col])
+        df[col].loc[df[col]>df[col].mean()*3] = Q1 + 0.4*IQR
+        print("after fix:", df.iloc[i][col])
+    print(df.describe())
+    return df
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+
+    return False
+
+def process_title(combined):
+    # 从姓名中提取称谓的函数
+    def get_title(name):
+        # 正则表达式检索称谓，称谓总以大写字母开头并以句点结尾
+        title_search = re.search(' ([A-Za-z]+)\.', name)
+        # 如果称谓存在则返回其值
+        if title_search:
+            return title_search.group(1)
+        return ""
+
+    # 将每个称谓映射到一个整数，有些太少见的称谓可以压缩到一个数值
+    #mlle:miss, Master:院长 Rev:牧师 Major:少将 Col:上校 Mme:Mrs, Don:大学教师 Layd: 贵族女眷或夫人 
+    #Countess:女伯爵 Jonkheer:最低等级的贵族（男） Sir:爵士#{"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 5, "Major": 5, "Col": 5,
+    #"Mlle": 2, "Mme": 3, "Don": 6, "Lady": 6, "Countess": 6, "Jonkheer": 6, "Sir": 6, "Capt": 5, "Ms": 3, "Dona": 6}
+    def map_title(title):
+        title_mapping = {
+                            "Capt":       "Officer",
+                            "Col":        "Officer",
+                            "Major":      "Officer",
+                            "Jonkheer":   "Royalty",
+                            "Don":        "Royalty",
+                            "Sir" :       "Royalty",
+                            "Dr":         "Officer",
+                            "Rev":        "Officer",
+                            "Countess":   "Royalty",
+                            "Dona":       "Royalty",
+                            "Mme":        "Mrs",
+                            "Mlle":       "Miss",
+                            "Ms":         "Mrs",
+                            "Mr" :        "Mr",
+                            "Mrs" :       "Mrs",
+                            "Miss" :      "Miss",
+                            "Master" :    "Master",
+                            "Lady" :      "Royalty"
+            }
+        if title in title_mapping:
+            return title_mapping[title]
+        print(title, "not in title_mapping")
+        return 0
+
+    combined["Title"] = combined["Name"].apply(get_title).apply(map_title)
+
+def process_age(combined, grouped_median_age):
+    # a function that fills the missing values of the Age variable
+    def fillAges(row, grouped_median):
+        return grouped_median.loc[row['Sex'], row['Pclass'], row['Title']]['Age']
+    combined.Age = combined.apply(lambda r : fillAges(r, grouped_median_age) if np.isnan(r['Age']) 
+                                                      else r['Age'], axis=1)
+
+def process_cabin(combined, grouped_mode_cabin):
+    # a function that fills the missing values of the Age variable
+    def fillCabin(row, grouped_mode):
+        return grouped_mode.loc[row['Pclass']]['Cabin'][0]
+    combined.Cabin = combined.apply(lambda r :  fillCabin(r, grouped_mode_cabin) if pd.isnull(r['Cabin']) else r['Cabin'], axis=1)       #1 or ‘columns’: apply function to each row
+
+
+#process missing data
+def TitanicImputer(X):
+    print("before Imputer\n", X.describe())
+    #Embarked
+    X['Embarked'].fillna(X['Embarked'].mode()[0], inplace=True)
+    #Fare
+    X['Fare'].fillna(X['Fare'].median(), inplace=True)
+    #Title
+    process_title(X)
+    #Age
+    grouped_age = X.groupby(['Sex','Pclass','Title'])
+    grouped_median_age = grouped_age.median()
+    #print(grouped_median_age)
+    process_age(X, grouped_median_age)
+    #Cabin
+    grouped_cabin = X.groupby(['Pclass'])   #returned DataFrameGroupBy
+    grouped_mode_cabin = grouped_cabin.apply(lambda x:x.mode()) #returned DataFrame
+    #print(grouped_mode_cabin.loc[1]['Cabin'][0])
+    #print(grouped_mode_cabin.loc[2]['Cabin'][0])
+    #print(grouped_mode_cabin.loc[3]['Cabin'][0])
+    process_cabin(X, grouped_mode_cabin)
+    #print("check NaN after Imputer:\n", X.isnull().any())
+    if X.isnull().any().any():
+        print('Nan After Imputer, exit!', X.isnull().any())
+        exit()
+    return X
+
+#clean data, and extract features 
+def DataFrameMangle(X):
+    #global mangled_columns
+    X['FamilySize'] = X['SibSp'] + X['Parch'] + 1
+    X['IsAlone'] = 1
+    X['IsAlone'].loc[X['FamilySize']>1] = 0
+    #X['Fare'] = fix_outliers(X, 'Fare')
+    #qcut: 按区间内的样本数均分，cut: 按区间均分
+    X["FareBin"] = pd.qcut(X["Fare"], 4, labels=False)  #X["FareBin"] = pd.qcut(X["Fare"], 4, labels=["a", "b", "c", "d"]/False)
+    X["AgeBin"] = pd.cut(X["Age"].astype(int), 5, labels=False)
+    X.drop('Name', axis=1, inplace=True)
+    X.drop('Fare', axis=1, inplace=True)
+    X.drop('Age', axis=1, inplace=True)
+    return X
+
+#encode label, adjust to estimator
+def DataFrameEncode(X):
+    def OneHotEncodeCategory(X):
+        for col in X:
+            #print(col, X[col].dtype, is_number(X[col].iloc[0]))
+            if not is_number(X[col].iloc[0]):
+                col_dummies = pd.get_dummies(X[col], prefix=col)
+                X = pd.concat([X, col_dummies], axis=1)
+                X.drop(col, axis=1, inplace=True)
+        return X
+    def LabelEncodeCategory(X):
+        le = LabelEncoder()     # this can only be used for tree-based estimator, because of no sense of order
+        for col in X:
+            if not is_number(X[col].iloc[0]):
+                X[col] = le.fit_transform(X[col])
+        return X
+    X["Sex"] = X["Sex"].map({'male':1,'female':0})
+    X["Cabin"] = X["Cabin"].map(lambda c:c[0])
+    #X = OneHotEncodeCategory(X)
+    X = LabelEncodeCategory(X)     #only for tree estimator
+    #print("after encode: head,tail 2rows\n", X.head(2), X.tail(2))
+    return X
+
+def DataFrameLoad():
+    print(check_output(["ls", "input/"]).decode("utf8"))
+    # Any results you write to the current directory are saved as output.
+    train = pd.read_csv("input/train.csv", header=0)
+    test = pd.read_csv("input/test.csv", header=0)
+    #print(train.info())
+    #print(test.info())
+    feature_to_use = ["Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Fare", "Cabin", "Embarked"]
+    combined = train[feature_to_use].append(test[feature_to_use])
+    #print(train.describe())
+    #print(test.describe())
+    #print(combined.describe())
+    return (combined, train, test)
+
+#与模型无关的选择特征
+def get_k_best_feature(train, target, K=10):
+    selector = SelectKBest(f_classif, k=K)   #http://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.SelectKBest.html#sklearn.feature_selection.SelectKBest
+    selector.fit(train, target)
+    # 得到每个特征列的p值，再转换为交叉验证得分
+    scores = -np.log10(selector.pvalues_)
+    print(sorted(scores, reverse=True), scores.mean(), '\n--------------')
+    selector_feature = np.argsort(scores, axis=0)[:-(K+1):-1] 
+    selected_columns = [mangled_columns[x] for x in selector_feature]
+    print([(mangled_columns[col], scores.tolist()[col]) for col in selector_feature])
+    print("\nselected columns:", K)
+    print(selected_columns)
+    return (selector, selected_columns)
+
+combined, train, test = DataFrameLoad()
+target = train['Survived']
+test_pid = test['PassengerId']
+combined_imputed = TitanicImputer(combined)
+combined_mangled = DataFrameMangle(combined_imputed)
+combined_encoded = DataFrameEncode(combined_mangled)
+train = combined_encoded[:train.shape[0]]
+test = combined_encoded[train.shape[0]:]
+mangled_columns = combined_encoded.columns.values.tolist()
+print("train/test rows: {}, {}, prepared columns: {}\n".format(train.shape[0], test.shape[0], len(mangled_columns)), mangled_columns)
+
+train_M = train.as_matrix()
+test_M = test.as_matrix()
+
+featureFromModel = False
+if not featureFromModel: 
+    selected_feature_num = 10
+    selector,selected_columns = get_k_best_feature(train, target, selected_feature_num)
+    combined_selected = selector.transform(combined_encoded)
+    train_S = combined_selected[:train.shape[0]]
+    test_S = combined_selected[train.shape[0]:]
+
+    model,prediction,m_name = ensemble_model(train_S, target, test_S, selected_columns, featureFromModel)
+else:
+    model,prediction,m_name = ensemble_model(train_M, target, test_M, mangled_columns, featureFromModel)
+    train_S = train_M 
+    test_S = test_M
+    selected_feature_num = "fromMode"
+
+#m_name = "xgbc_"
+#model = xgb.XGBClassifier(max_depth=8, n_estimators=100, learning_rate=0.05)
+#model.fit(train_S, train_y)
+#prediction = model.predict(test_S)
+
+#model must have fit method
+scores = cross_val_score(model, train_S, target, cv=3)
+print(scores)
+print(scores.mean())
+
+submission_name = m_name + '.csv'
+submission = pd.DataFrame({"PassengerId": test_pid, "Survived": prediction})
+submission.to_csv(submission_name, index=False)
