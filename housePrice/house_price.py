3f94a0757c33fae19e3ecc9c0f8b75f8cde2b505
newway <liuwei345@gmail.com>
2018-01-14 23:51:27 +0800

    wipe off outliers, clean code AverageModel	test cv:0.109453    train cv: 0.0735174	    leadboard:0.11500

--- housePrice/house_price.py
+++ housePrice/house_price.py
@@ -12,7 +12,7 @@ from sklearn.ensemble import RandomForestRegressor,  GradientBoostingRegressor
 import lightgbm as lgb
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import RobustScaler, LabelEncoder, normalize
+from sklearn.preprocessing import RobustScaler, LabelEncoder, normalize, MinMaxScaler
 from sklearn.feature_selection import SelectKBest, f_regression, mutual_info_regression
 from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, Lasso, LassoCV, LassoLarsCV
 from sklearn.model_selection import cross_val_score, GridSearchCV, KFold, ShuffleSplit
@@ -27,12 +27,17 @@ def DataFrameLoad():
     train = pd.read_csv("input/train.csv", header=0)
     test = pd.read_csv("input/test.csv", header=0)
     # drop outliers
+    #print(train[['OverallQual', 'SalePrice']].groupby(['OverallQual']).describe())
+    #print(train[['GrLivArea', 'SalePrice']].groupby(['GrLivArea']).describe())
     train.drop(train[(train['GrLivArea']>4000) & (train['SalePrice']<300000)].index, inplace=True)
+    train.drop(train[(train['OverallQual']<5) & (train['SalePrice']>200000)].index, inplace=True)
+    train.drop(train[(train['OverallQual']<=2) & (train['SalePrice']>=60000)].index, inplace=True)
+    train.drop(train[(train['OverallQual']==10) & (train['SalePrice']<170000)].index, inplace=True)
+    #print(train[['OverallQual', 'SalePrice']].groupby(['OverallQual']).describe())
+    #exit()
     #print(train.info())     #1460 r
     #print(test.info())      #1459 r
     #train.drop(miss_delete_columns, axis=1, inplace=True)
-    #miss_delete_columns = get_missing_rate(test)
-    #test.drop(miss_delete_columns, axis=1, inplace=True)
     feature_to_use = [ col for col in train.columns if col in test.columns]
     combined = train[feature_to_use].append(test[feature_to_use])
     get_missing_rate(combined)
@@ -122,10 +127,7 @@ def DataMangle(X):
     #X['YrSold'] = X['YrSold'].astype(str)
     #X['MoSold'] = X['MoSold'].astype(str)
     X['YrSold'] = X['YrSold'] + X['MoSold']/12.0
-    #X['YrSold'] = X.apply(lambda r:r['YrSold'] + r['MoSold']/12.0)
-    #print(X['YrSold'].head(3))
     # add one column total square feet
-    #X['TotalSF'] = X['TotalBsmtSF'] + X['1stFlrSF'] + X['2ndFlrSF']
     X.drop(["Id",'MoSold'], axis = 1, inplace = True)
     #normalizing data
     numeric_feat = X.dtypes[X.dtypes != "object"].index
@@ -136,8 +138,7 @@ def DataMangle(X):
     lam = 0.15
     X[skewed_feats] = boxcox1p(X[skewed_feats], lam)
     # Adding total sqfootage feature
-    #print(X.columns.tolist())
-    X[numeric_feat] = RobustScaler().fit_transform(X[numeric_feat])
+    X[numeric_feat] = RobustScaler().fit_transform(X[numeric_feat]) #
     return X
 
 def map_category_by_target(train, test, tg_name, target, c):
@@ -166,7 +167,7 @@ def DataEncoder(X):
     cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond',
         'ExterQual', 'ExterCond','HeatingQC', 'KitchenQual', 'BsmtFinType1',
         'BsmtFinType2', 'Functional', 'BsmtExposure', 'GarageFinish', 'LandSlope',
-        'LotShape', 'PavedDrive', 'Street', 'CentralAir', 'MSSubClass', 'OverallCond')   #, 'YrSold', 'MoSold','PoolQC', 'MiscFeature', 'Alley', 'Fence'
+        'LotShape', 'PavedDrive', 'Street', 'CentralAir', 'MSSubClass', 'OverallCond')   #,'OverallQual', 'YrSold', 'MoSold','PoolQC', 'MiscFeature', 'Alley', 'Fence'
     #cols = X.select_dtypes(include = ["object"]).columns
     # process columns, apply LabelEncoder to categorical features
     for c in cols:
@@ -174,10 +175,8 @@ def DataEncoder(X):
         #X[c] = X[c].map(cate_map)
         lbl = LabelEncoder()
         X[c] = lbl.fit_transform(X[c])
-
     X = pd.get_dummies(X)
     print(X.shape)
-    #print(X.head(2))
     return X
 
 def rmse_cv(model, data, target, cv):
@@ -188,6 +187,17 @@ def rmse_cv(model, data, target, cv):
 def rmsle(y, y_pred):
     return np.sqrt(mean_squared_error(y, y_pred))
 
+def get_K_corr(X, y, k=20):
+    train_corr = X.copy() 
+    train_corr['target-'] = y
+    corrmat = train_corr.corr()     #train_corr.corrwith(y)
+    cor_feat = corrmat['target-'].drop(labels='target-').sort_values(axis=0, ascending=False)
+    #X.drop(cor_feat[abs(cor_feat) < 0.15].index, axis=1, inplace=True)
+    cor_feat = cor_feat[abs(cor_feat) > 0.15]
+    print(cor_feat, len(cor_feat))
+    #return cor_feat[abs(cor_feat) < 0.15].index
+    return cor_feat.index.tolist()[:k]
+
 combined, train, test = DataFrameLoad()
 train['SalePrice'] = np.log1p(train['SalePrice'])
 target = train['SalePrice'].as_matrix()
@@ -195,77 +205,15 @@ test_Id = test.Id
 DataImpute(combined)
 # why log of target:price, because price is a multiple mode of all factors, log transform make it linear
 combined = DataMangle(combined)
+train_df = combined[:train.shape[0]]
+test_df = combined[train.shape[0]:]
+#key_fe = get_K_corr(train_df, train['SalePrice'], k=25)
+#combined.drop(key_fe, axis=1, inplace=True)
 combined = DataEncoder(combined)
-#train_corr = pd.concat([combined[:train.shape[0]], train.SalePrice], axis=1)
-#corrmat = train_corr.corr()
-#cor_feat = corrmat.SalePrice.sort_values(axis=0, ascending=False)
-#cor_feat = cor_feat[abs(cor_feat) > 0.15].index
-#print(cor_feat, len(cor_feat))
-#cor_set = set(cor_feat.tolist())
+all_columns = combined.columns
 train_M = combined[:train.shape[0]].as_matrix()
 test_M = combined[train.shape[0]:].as_matrix()
 #print(np.any(np.isinf(train_M)), np.any(np.isinf(target)))
-
-#alphas = [0.05, 0.1, 0.3, 1, 3, 5, 10, 15, 30, 50, 75]
-#cv_ridge = [rmse_cv(Ridge(alpha = alpha), train_M, target, 5).mean()
-#            for alpha in alphas]
-#cv_ridge = pd.Series(cv_ridge, index = alphas)
-#print(cv_ridge.values, cv_ridge.min())
-
-alpha =0.0005
-
-#model_xgb.fit(train_M, target)
-#GBoost.fit(train_M, target)
-#ENet.fit(train_M, target)
-#KRR.fit(train_M, target)
-#score =rmse_cv(model_xgb, train_M, target, 5) 
-#print(score.mean(), score.std(), score.mean()+3*score.std())
-#prediction = model_xgb.predict(train_M)
-#print("xgb:", rmsle(target, prediction))
-
-#model_lgb.fit(train_M, target)
-#score =rmse_cv(model_lgb, train_M, target, 5) 
-#print(score.mean(), score.std(), score.mean()+3*score.std())
-#prediction = model_lgb.predict(train_M)
-#print("lgb:", rmsle(target, prediction))
-
-#model_lasso = LassoCV(alphas=[0.1, 0.001, 0.0005, 0.0006, 0.0008]).fit(train_M, target)
-#coef = pd.Series(model_lasso.coef_, index = combined.columns)
-#lasso_pick_num = sum(coef != 0)
-#lasso_pick_index = coef[coef!=0].index
-#score =rmse_cv(model_lasso, train_M, target, 5)
-#print("Lasso picked " + str(lasso_pick_num) + " variables and eliminated the other " +  str(sum(coef == 0)) + " variables")
-#print(score.mean(), score.std(), score.mean()+3*score.std(), model_lasso.alpha_)
-#prediction = np.expm1(model_lasso.predict(test_M))
-
-#averaged_models = AveragingModels(models=None)
-#averaged_models.fit(train_M, target)
-#score = rmse_cv(averaged_models, train_M, target, 5)
-#print("Averaged models score: {:.6f} ({:.6f}), worst:{:.6f}, number:{}".format(score.mean(), score.std(), score.mean()+3*score.std(), len(averaged_models.models_)))
-#prediction = np.expm1(averaged_models.predict(test_M))
-#print("Averaged models train rmsle:", rmsle(target, averaged_models.predict(train_M)))
-#prediction = np.expm1(averaged_models.predict(test_M))
-
-#stacked_averaged_models = StackingAveragedModels(base_models=None, meta_model=None)
-#stacked_averaged_models.fit(train_M, target)
-#score = rmse_cv(stacked_averaged_models, train_M, target, 5)
-#weights = stacked_averaged_models.meta_model_.coef_
-#print("stack-avg parameter vector (w in the cost function formula):", stacked_averaged_models.meta_model_.coef_)
-#print("stacked-avg models score: {:.6f} ({:.6f}), worst:{:.6f}".format(score.mean(), score.std(), score.mean()+3*score.std()))
-#train_pred = stacked_averaged_models.predict(train_M)
-#print("stack meta model rmsle :", rmsle(target, train_pred))
-#prediction = np.expm1(stacked_averaged_models.predict(test_M))
-
-
-#xgb_pred = model_xgb.predict(test_M)
-#gbt_pred = GBoost.predict(test_M)
-#enet_pred = ENet.predict(test_M)
-#krr_pred = KRR.predict(test_M)
-#xgb_pred_e = np.expm1(xgb_pred)
-#gbt_pred_e = np.expm1(gbt_pred)
-#enet_pred_e = np.expm1(enet_pred)
-#krr_pred_e = np.expm1(krr_pred)
-#
 
 score_f = make_scorer(rmsle, greater_is_better=False)
 param_house = {
@@ -287,7 +235,7 @@ param_house = {
 }
 cv_split = ShuffleSplit(n_splits = 10, test_size = 0.3, train_size = 0.6, random_state = 0 ) # run model 10x with 60/30 split intentionally leaving out 10
 #cv_split = KFold(5, shuffle=True, random_state=42)
-model,prediction,m_name = ensemble_regression(train_M, target, test_M, combined.columns, cv_split=cv_split, score_f=score_f, EST_PARAM_GRID=param_house)    #, EST_PARAM_GRID=param_grid
+model,prediction,m_name = ensemble_regression(train_M, target, test_M, all_columns, cv_split=cv_split, score_f=score_f, EST_PARAM_GRID=param_house)    #, EST_PARAM_GRID=param_grid
 submission = pd.DataFrame({"Id": test_Id, "SalePrice": np.expm1(prediction)})
 subname = m_name + '.csv'
 submission.to_csv(subname, index=False)
